03863aae0a0fe4495a3a7e8fb83394d55fbf54ec
EmediongPeter <emedionguseh18@gmail.com>
2024-10-07 13:35:27 +0100

    compiled

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'C:/Users/user/Projects/NestJS/anvith_backend/node_modules/rxjs';
+import { Observable } from 'rxjs';
 import { IS_ADMIN_KEY } from '../decorators/is-admin.decorator';
 import { userTypes } from 'src/modules/users/schemas/user.schema';
 
